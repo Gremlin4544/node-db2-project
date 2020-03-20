4d7b61b2a2edda181ed92e3b4d87a54d475d0998
Gremlin4544 <alve@mckethen.com>
2020-03-19 20:02:03 -0400

    added index.js file and cars table

--- data/migrations/20200319195415_create-cars-table.js
+++ data/migrations/20200319195415_create-cars-table.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
+    //the change we want to make to our schema
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('vin').notNullable().index();
+        tbl.string('make').notNullable().index();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('transmission');
+        tbl.string('status');
+        tbl.timestamps(true, true);
+    });
   
 };
 
 exports.down = function(knex) {
-  
+    //undoing that change
+    return knex.schema.dropTableIfExists('cars-table');
 };
